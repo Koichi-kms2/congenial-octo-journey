1702428a32036fa488a70655f82c778c7461ed41
Koichi-kms2 <satou_ce@kms2.co.jp>
2019-01-01 10:41:54 +0900

    一旦ベースいじったやつを反映　佐藤１号

--- html5up-verti/assets/js/main.js
+++ html5up-verti/assets/js/main.js
@@ -14,7 +14,7 @@
 			xlarge:  [ '1281px',  '1680px' ],
 			large:   [ '981px',   '1280px' ],
 			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
+			small:   [ null,      '600px'  ]
 		});
 
 	// Play initial animations on page load.
